17d6bcec61170b1a27d51da73fec14a6bc56c632
Holger Oehm <holger.oehm@holger-oehm.de>
2013-05-09 16:58:42 +0200

    refactored radicalReOrder
    extracted the different cases as methods

--- slicer/src/main/java/org/reprap/geometry/polygons/PolygonList.java
+++ slicer/src/main/java/org/reprap/geometry/polygons/PolygonList.java
@@ -321,127 +321,141 @@ public class PolygonList {
         }
 
         // First check that we all have the same material
-        final String material = polygon(0).getMaterial();
-        for (int i = 1; i < size(); i++) {
-            if (!material.equals(polygon(i).getMaterial())) {
-                throw new RuntimeException("RrPolygonList.radicalReOrder(): more than one material in the list!");
-            }
-        }
+        validateSameMaterial();
 
         // Now go through the polygons pairwise
         for (int i = 0; i < size() - 1; i++) {
-            Polygon myPolygon = polygon(i);
             for (int j = i + 1; j < size(); j++) {
-                double d = Double.POSITIVE_INFINITY;
-                double d2;
-                int myPoint = -1;
-                int itsPoint = -1;
-                int myTempPoint, itsTempPoint;
-                boolean reverseMe, reverseIt;
-                Polygon itsPolygon = polygon(j);
-
                 // Swap the odd half of the asymmetric cases so they're all the same
-                if (myPolygon.isClosed() && !itsPolygon.isClosed()) {
-                    polygons.set(i, itsPolygon);
-                    polygons.set(j, myPolygon);
-                    myPolygon = polygon(i);
-                    itsPolygon = polygon(j);
+                if (polygon(i).isClosed() && !polygon(j).isClosed()) {
+                    final Polygon left = polygon(i);
+                    final Polygon right = polygon(j);
+                    polygons.set(i, right);
+                    polygons.set(j, left);
                 }
 
+                final boolean leftClosed = polygon(i).isClosed();
+                final boolean rightClosed = polygon(j).isClosed();
                 // Three possibilities ...
-                if (!myPolygon.isClosed() && !itsPolygon.isClosed()) {
-                    // ... both open
-                    // Just compare the four ends
-                    reverseMe = true;
-                    reverseIt = false;
-                    d = Point2D.dSquared(myPolygon.point(0), itsPolygon.point(0));
-
-                    d2 = Point2D.dSquared(myPolygon.point(myPolygon.size() - 1), itsPolygon.point(0));
-                    if (d2 < d) {
-                        reverseMe = false;
-                        reverseIt = false;
-                        d = d2;
-                    }
-
-                    d2 = Point2D.dSquared(myPolygon.point(0), itsPolygon.point(itsPolygon.size() - 1));
-                    if (d2 < d) {
-                        reverseMe = true;
-                        reverseIt = true;
-                        d = d2;
-                    }
-
-                    d2 = Point2D.dSquared(myPolygon.point(myPolygon.size() - 1), itsPolygon.point(itsPolygon.size() - 1));
-                    if (d2 < d) {
-                        reverseMe = false;
-                        reverseIt = true;
-                        d = d2;
-                    }
-
-                    if (d < linkUp) {
-                        if (reverseMe) {
-                            myPolygon = myPolygon.negate();
-                        }
-                        if (reverseIt) {
-                            itsPolygon = itsPolygon.negate();
-                        }
-                        myPolygon.add(itsPolygon);
-                        polygons.set(i, myPolygon);
-                        polygons.remove(j);
-                    }
-
-                } else if (!myPolygon.isClosed() && itsPolygon.isClosed()) {
-                    // ... I'm open, it's closed;
-                    // Compare my end points with all its points
-                    reverseMe = true;
-                    itsPoint = itsPolygon.nearestVertex(myPolygon.point(0));
-                    d = Point2D.dSquared(itsPolygon.point(itsPoint), myPolygon.point(0));
-                    itsTempPoint = itsPolygon.nearestVertex(myPolygon.point(myPolygon.size() - 1));
-                    d2 = Point2D.dSquared(itsPolygon.point(itsTempPoint), myPolygon.point(myPolygon.size() - 1));
-                    if (d2 < d) {
-                        itsPoint = itsTempPoint;
-                        reverseMe = false;
-                        d = d2;
-                    }
-
-                    if (d < linkUp) {
-                        itsPolygon = itsPolygon.newStart(itsPoint);
-                        itsPolygon.add(itsPolygon.point(0)); // Make sure the second half really is closed
-                        if (reverseMe) {
-                            myPolygon = myPolygon.negate();
-                        }
-                        myPolygon.add(itsPolygon);
-                        myPolygon.setOpen(); // We were closed, but we must now be open
-                        polygons.set(i, myPolygon);
-                        polygons.remove(j);
-                    }
-
-                } else if (myPolygon.isClosed() && itsPolygon.isClosed()) {
-                    // ... both closed
-                    // Compare all my points with all its points
-                    for (int k = 0; k < itsPolygon.size(); k++) {
-                        myTempPoint = myPolygon.nearestVertex(itsPolygon.point(k));
-                        d2 = Point2D.dSquared(myPolygon.point(myTempPoint), itsPolygon.point(k));
-                        if (d2 < d) {
-                            myPoint = myTempPoint;
-                            itsPoint = k;
-                            d = d2;
-                        }
-                    }
-
-                    if (d < linkUp) {
-                        myPolygon = myPolygon.newStart(myPoint);
-                        myPolygon.add(myPolygon.point(0)); // Make sure we come back to the start
-                        itsPolygon = itsPolygon.newStart(itsPoint);
-                        itsPolygon.add(itsPolygon.point(0)); // Make sure we come back to the start
-                        myPolygon.add(itsPolygon);
-                        //myPolygon.setOpen(); // We were closed, but we must now be open
-                        polygons.set(i, myPolygon);
-                        polygons.remove(j);
-                    }
-
+                if (!leftClosed && !rightClosed) {
+                    handleTwoOpenPolygons(linkUp, i, j);
+                } else if (!leftClosed && rightClosed) {
+                    handleOpenAndClosedPolygon(linkUp, i, j);
+                } else if (leftClosed && rightClosed) {
+                    handleTwoClosedPolygons(linkUp, i, j);
                 } else {
                     throw new RuntimeException("RrPolygonList.radicalReOrder(): Polygons are neither closed nor open!");
                 }
+            }
+        }
+    }
+
+    private void handleTwoClosedPolygons(final double linkUp, final int i, final int j) {
+        Polygon myPolygon = polygon(i);
+        Polygon itsPolygon = polygon(j);
+        int myPoint = -1;
+        int itsPoint = -1;
+        double d = Double.POSITIVE_INFINITY;
+        for (int k = 0; k < itsPolygon.size(); k++) {
+            final int myTempPoint = myPolygon.nearestVertex(itsPolygon.point(k));
+            final double d2 = Point2D.dSquared(myPolygon.point(myTempPoint), itsPolygon.point(k));
+            if (d2 < d) {
+                myPoint = myTempPoint;
+                itsPoint = k;
+                d = d2;
+            }
+        }
+
+        if (d < linkUp) {
+            myPolygon = myPolygon.newStart(myPoint);
+            myPolygon.add(firstPointOf(myPolygon)); // Make sure we come back to the start
+            itsPolygon = itsPolygon.newStart(itsPoint);
+            itsPolygon.add(firstPointOf(itsPolygon)); // Make sure we come back to the start
+            myPolygon.add(itsPolygon);
+            polygons.set(i, myPolygon);
+            polygons.remove(j);
+        }
+    }
+
+    private void handleOpenAndClosedPolygon(final double linkUp, final int i, final int j) {
+        Polygon myPolygon = polygon(i);
+        Polygon itsPolygon = polygon(j);
+        int itsPoint = itsPolygon.nearestVertex(firstPointOf(myPolygon));
+        double d = Point2D.dSquared(itsPolygon.point(itsPoint), firstPointOf(myPolygon));
+        final int itsTempPoint = itsPolygon.nearestVertex(lastPointOf(myPolygon));
+        final double d2 = Point2D.dSquared(itsPolygon.point(itsTempPoint), lastPointOf(myPolygon));
+        boolean reverseMe = true;
+        if (d2 < d) {
+            itsPoint = itsTempPoint;
+            reverseMe = false;
+            d = d2;
+        }
+
+        if (d < linkUp) {
+            itsPolygon = itsPolygon.newStart(itsPoint);
+            itsPolygon.add(firstPointOf(itsPolygon)); // Make sure the second half really is closed
+            if (reverseMe) {
+                myPolygon = myPolygon.negate();
+            }
+            myPolygon.add(itsPolygon);
+            myPolygon.setOpen(); // We were closed, but we must now be open
+            polygons.set(i, myPolygon);
+            polygons.remove(j);
+        }
+    }
+
+    private static Point2D firstPointOf(final Polygon polygon) {
+        return polygon.point(0);
+    }
+
+    private static Point2D lastPointOf(final Polygon polygon) {
+        return polygon.point(polygon.size() - 1);
+    }
+
+    private void handleTwoOpenPolygons(final double linkUp, final int i, final int j) {
+        Polygon myPolygon = polygon(i);
+        Polygon itsPolygon = polygon(j);
+        boolean reverseMe = true;
+        boolean reverseIt = false;
+        double d = Point2D.dSquared(firstPointOf(myPolygon), firstPointOf(itsPolygon));
+        double d2 = Point2D.dSquared(lastPointOf(myPolygon), firstPointOf(itsPolygon));
+        if (d2 < d) {
+            reverseMe = false;
+            reverseIt = false;
+            d = d2;
+        }
+
+        d2 = Point2D.dSquared(firstPointOf(myPolygon), lastPointOf(itsPolygon));
+        if (d2 < d) {
+            reverseMe = true;
+            reverseIt = true;
+            d = d2;
+        }
+
+        d2 = Point2D.dSquared(lastPointOf(myPolygon), lastPointOf(itsPolygon));
+        if (d2 < d) {
+            reverseMe = false;
+            reverseIt = true;
+            d = d2;
+        }
+
+        if (d < linkUp) {
+            if (reverseMe) {
+                myPolygon = myPolygon.negate();
+            }
+            if (reverseIt) {
+                itsPolygon = itsPolygon.negate();
+            }
+            myPolygon.add(itsPolygon);
+            polygons.set(i, myPolygon);
+            polygons.remove(j);
+        }
+    }
+
+    private void validateSameMaterial() {
+        for (int i = 1; i < size(); i++) {
+            if (!polygon(0).getMaterial().equals(polygon(i).getMaterial())) {
+                throw new RuntimeException("RrPolygonList.radicalReOrder(): more than one material in the list!");
             }
         }
     }
