2eafc904bab36a66590d0855023d34e15f94ca21
Holger Oehm <holger.oehm@holger-oehm.de>
2013-05-12 12:54:04 +0200

    added more actions and use them for both menu and buttons
    * load RFO
    * load STL/CSG

--- slicer/src/main/java/org/reprap/gui/MainFrame.java
+++ slicer/src/main/java/org/reprap/gui/MainFrame.java
@@ -22,8 +22,13 @@ import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -31,18 +36,28 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.WindowConstants;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import org.reprap.configuration.Configuration;
 
 public class MainFrame extends JFrame {
+    static final String LOAD_RFO_ACTION = "Load RFO";
+    static final String SAVE_RFO_ACTION = "Save RFO";
+    static final String LOAD_STL_CSG_ACTION = "Load STL/CSG";
+    static final String SLICE_ACTION = "Slice";
+    static final String EXIT_ACTION = "Exit";
+
     private final Configuration configuration;
     private final RepRapPlater plater;
+    private final Map<String, Action> actions = new HashMap<String, Action>();
 
     public MainFrame() throws HeadlessException {
         super("RepRap Slicer");
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         configuration = Configuration.create();
         plater = new RepRapPlater(configuration.getCurrentConfiguration());
+        createActions();
     }
 
     public void createGui() {
@@ -60,9 +75,67 @@ public class MainFrame extends JFrame {
         });
     }
 
+    private void createActions() {
+        actions.put(LOAD_RFO_ACTION, new AbstractAction(LOAD_RFO_ACTION) {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                load("RFO multiple-object file", new String[] { "rfo" }, "");
+            }
+        });
+        actions.put(SAVE_RFO_ACTION, new AbstractAction(SAVE_RFO_ACTION) {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                // TODO Auto-generated method stub
+                System.out.println(SAVE_RFO_ACTION);
+            }
+        });
+        actions.put(LOAD_STL_CSG_ACTION, new AbstractAction(LOAD_STL_CSG_ACTION) {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                load("STL triangulation file", new String[] { "stl" }, "");
+            }
+        });
+        actions.put(SLICE_ACTION, new AbstractAction(SLICE_ACTION) {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                // TODO Auto-generated method stub
+                System.out.println(SLICE_ACTION);
+            }
+        });
+        actions.put(EXIT_ACTION, new AbstractAction(EXIT_ACTION) {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                dispose();
+            }
+        });
+    }
+
+    private File load(final String description, final String[] extensions, final String defaultName) {
+        final FileFilter filter = new FileNameExtensionFilter(description, extensions);
+        final JFileChooser chooser = new JFileChooser();
+        chooser.setFileFilter(filter);
+        if (!defaultName.isEmpty() && extensions.length == 1) {
+            final File defaultFile = new File(defaultName + "." + extensions[0]);
+            chooser.setSelectedFile(defaultFile);
+        }
+
+        final int returnVal = chooser.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            final File result = chooser.getSelectedFile();
+            if (extensions[0].toUpperCase().contentEquals("RFO")) {
+                plater.loadRFOFile(result);
+            }
+            if (extensions[0].toUpperCase().contentEquals("STL")) {
+                plater.anotherSTLFile(result);
+            }
+            return result;
+        }
+        return null;
+    }
+
     private JTabbedPane createTabPane() {
         final JTabbedPane tabPane = new JTabbedPane();
-        tabPane.add("Plater", new PlaterPanel(configuration, plater));
+        tabPane.add("Plater", new PlaterPanel(configuration, plater, actions));
         tabPane.add("Print Settings", new PrintSettingsPanel());
         tabPane.add("Material Settings", new JPanel());
         tabPane.add("Printer Settings", new JPanel());
@@ -83,13 +156,11 @@ public class MainFrame extends JFrame {
 
     private JMenu createFileMenu() {
         final JMenu fileMenu = new JMenu("File");
-        final JMenuItem quit = new JMenuItem(new AbstractAction("Exit") {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                dispose();
-            }
-        });
-        fileMenu.add(quit);
+        fileMenu.add(new JMenuItem(actions.get(LOAD_RFO_ACTION)));
+        fileMenu.add(new JMenuItem(actions.get(SAVE_RFO_ACTION)));
+        fileMenu.add(new JMenuItem(actions.get(LOAD_STL_CSG_ACTION)));
+        fileMenu.addSeparator();
+        fileMenu.add(new JMenuItem(actions.get(EXIT_ACTION)));
         return fileMenu;
     }
 }
--- slicer/src/main/java/org/reprap/gui/PlaterPanel.java
+++ slicer/src/main/java/org/reprap/gui/PlaterPanel.java
@@ -21,10 +21,11 @@ package org.reprap.gui;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.util.Map;
 
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 import javax.swing.border.EtchedBorder;
 
 import org.reprap.configuration.Configuration;
@@ -32,10 +33,8 @@ import org.reprap.configuration.Configuration;
 public class PlaterPanel extends JPanel {
 
     private static final Insets BIG_INSETS = new Insets(5, 5, 5, 5);
-    private final Configuration configuration;
 
-    public PlaterPanel(final Configuration configuration, final RepRapPlater plater) {
-        this.configuration = configuration;
+    public PlaterPanel(final Configuration configuration, final RepRapPlater plater, final Map<String, Action> actions) {
         setLayout(new GridBagLayout());
         final GridBagConstraints constraints = new GridBagConstraints();
         constraints.gridx = 0;
@@ -51,28 +50,23 @@ public class PlaterPanel extends JPanel {
         constraints.weightx = 0;
         add(new CurrentConfigurationStateBox(configuration), constraints);
         constraints.gridy++;
-        add(makeButtonsBox(), constraints);
+        add(makeButtonsBox(actions), constraints);
     }
 
-    private JPanel makePlaterBox(final RepRapPlater plater) {
+    private static JPanel makePlaterBox(final RepRapPlater plater) {
         plater.setBorder(new EtchedBorder());
         return plater;
     }
 
-    private JPanel makeButtonsBox() {
+    private static JPanel makeButtonsBox(final Map<String, Action> actions) {
         final JPanel buttonsBox = new JPanel();
         buttonsBox.setLayout(new GridBagLayout());
         buttonsBox.setBorder(new EtchedBorder());
-        addButtons(buttonsBox);
+        addButtons(buttonsBox, actions);
         return buttonsBox;
     }
 
-    private void addButtonsBox() {
-        final JPanel buttonsBox = makeButtonsBox();
-        add(buttonsBox);
-    }
-
-    private void addButtons(final JPanel buttonsBox) {
+    private static void addButtons(final JPanel buttonsBox, final Map<String, Action> actions) {
         final GridBagConstraints constraints = new GridBagConstraints();
         constraints.gridx = 0;
         constraints.gridy = 0;
@@ -80,62 +74,27 @@ public class PlaterPanel extends JPanel {
         constraints.ipady = 5;
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.insets = BIG_INSETS;
-        final JButton loadRFO = new JButton();
+        final JButton loadRFO = new JButton(actions.get(MainFrame.LOAD_RFO_ACTION));
         loadRFO.setBackground(new java.awt.Color(0, 204, 255));
-        loadRFO.setText("Load RFO");
-        loadRFO.setActionCommand("loadRFO");
-        loadRFO.addActionListener(new java.awt.event.ActionListener() {
-            @Override
-            public void actionPerformed(final java.awt.event.ActionEvent evt) {
-            }
-
-        });
         buttonsBox.add(loadRFO, constraints);
         constraints.gridy++;
 
-        final JButton saveRFO = new JButton();
+        final JButton saveRFO = new JButton(actions.get(MainFrame.SAVE_RFO_ACTION));
         saveRFO.setBackground(new java.awt.Color(0, 204, 255));
-        saveRFO.setText("Save RFO");
-        saveRFO.setActionCommand("saveRFO");
-        saveRFO.addActionListener(new java.awt.event.ActionListener() {
-            @Override
-            public void actionPerformed(final java.awt.event.ActionEvent evt) {
-            }
-        });
         buttonsBox.add(saveRFO, constraints);
         constraints.gridy++;
 
-        final JButton loadSTL = new JButton();
+        final JButton loadSTL = new JButton(actions.get(MainFrame.LOAD_STL_CSG_ACTION));
         loadSTL.setBackground(new java.awt.Color(0, 204, 255));
-        loadSTL.setText("Load STL/CSG");
-        loadSTL.setActionCommand("loadSTL");
-        loadSTL.addActionListener(new java.awt.event.ActionListener() {
-            @Override
-            public void actionPerformed(final java.awt.event.ActionEvent evt) {
-            }
-        });
         buttonsBox.add(loadSTL, constraints);
         constraints.gridy++;
 
-        final JButton sliceButton = new JButton();
+        final JButton sliceButton = new JButton(actions.get(MainFrame.SLICE_ACTION));
         sliceButton.setBackground(new java.awt.Color(51, 204, 0));
-        sliceButton.setText("Slice");
-        sliceButton.addActionListener(new java.awt.event.ActionListener() {
-            @Override
-            public void actionPerformed(final java.awt.event.ActionEvent evt) {
-            }
-        });
         buttonsBox.add(sliceButton, constraints);
         constraints.gridy++;
 
-        final JButton exitButton = new JButton();
-        exitButton.setText("Exit");
-        exitButton.addActionListener(new java.awt.event.ActionListener() {
-            @Override
-            public void actionPerformed(final java.awt.event.ActionEvent evt) {
-                SwingUtilities.getWindowAncestor(PlaterPanel.this).dispose();
-            }
-        });
+        final JButton exitButton = new JButton(actions.get(MainFrame.EXIT_ACTION));
         buttonsBox.add(exitButton, constraints);
         constraints.gridy++;
     }
