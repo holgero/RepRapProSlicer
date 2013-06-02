8e3e6b5c25cd835875dd5c3d4c4e8868e5a1bcf7
Holger Oehm <holger.oehm@holger-oehm.de>
2013-06-02 20:45:22 +0200

    refactorings in csg2d construction
    * removed indirection through index list, use points directly
    * removed flags[] array, introduced set of hull points instead
    * renamed polSection to findIndentation and extracted 3 methods

--- slicer/src/main/java/org/reprap/geometry/polygons/Polygon2Csg2dConverter.java
+++ slicer/src/main/java/org/reprap/geometry/polygons/Polygon2Csg2dConverter.java
@@ -20,7 +20,9 @@
 package org.reprap.geometry.polygons;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.reprap.configuration.store.MathRoutines;
 
@@ -31,7 +33,7 @@ class Polygon2Csg2dConverter {
         if (area(polygon) < 0) {
             this.polygon = polygon.negate();
         } else {
-            this.polygon = new Polygon(polygon);
+            this.polygon = polygon;
         }
     }
 
@@ -52,25 +54,20 @@ class Polygon2Csg2dConverter {
     }
 
     CSG2D getCsg2d() {
-        final List<Integer> all = allPoints();
-        final int[] flags = new int[polygon.size()];
-        return toCSGRecursive(all, 0, true, flags);
+        return toCSGRecursive(allPoints(), 0, true);
     }
 
     /**
      * Compute the CSG representation of a (sub)list recursively
      */
-    CSG2D toCSGRecursive(final List<Integer> a, int level, final boolean wrapAround, final int[] flags) {
-        flagSet(level, a, flags);
+    private static CSG2D toCSGRecursive(final List<Point2D> a, int level, final boolean wrapAround) {
         level++;
-        final List<Integer> ch = convexHull(a);
-        if (ch.size() < 3) {
-            throw new RuntimeException("null convex hull: " + ch.size() + " points.");
+        final Set<Point2D> hullSet = new HashSet<>(convexHull(a));
+        if (hullSet.size() < 3) {
+            throw new RuntimeException("invalid convex hull with " + hullSet.size() + " points.");
         }
 
-        flagSet(level, ch, flags);
         CSG2D hull;
-
         if (level % 2 == 1) {
             hull = CSG2D.universe();
         } else {
@@ -78,8 +75,7 @@ class Polygon2Csg2dConverter {
         }
 
         // Set-theoretically combine all the real edges on the convex hull
-        int i, oldi, flag, oldFlag, start;
-
+        int oldi, start;
         if (wrapAround) {
             oldi = a.size() - 1;
             start = 0;
@@ -88,32 +84,30 @@ class Polygon2Csg2dConverter {
             start = 1;
         }
 
-        for (i = start; i < a.size(); i++) {
-            oldFlag = flags[a.get(oldi).intValue()];
-            flag = flags[a.get(i).intValue()];
-
-            if (oldFlag == level && flag == level) {
-                final HalfPlane hp = new HalfPlane(listPoint(oldi, a), listPoint(i, a));
+        for (int i = start; i < a.size(); i++) {
+            if (hullSet.contains(a.get(oldi)) && hullSet.contains(a.get(i))) {
+                final HalfPlane hp = new HalfPlane(a.get(oldi), a.get(i));
                 if (level % 2 == 1) {
                     hull = CSG2D.intersection(hull, new CSG2D(hp));
                 } else {
                     hull = CSG2D.union(hull, new CSG2D(hp));
                 }
             }
-
             oldi = i;
         }
 
+        final Set<Point2D> doneWith = new HashSet<>(hullSet);
+
         // Finally deal with the sections on polygons that form the hull that
         // are not themselves on the hull.
-        List<Integer> section = polSection(a, level, flags);
-        while (section != null) {
+        while (a.size() > doneWith.size()) {
+            final List<Point2D> section = findIndentation(a, doneWith);
             if (level % 2 == 1) {
-                hull = CSG2D.intersection(hull, toCSGRecursive(section, level, false, flags));
+                hull = CSG2D.intersection(hull, toCSGRecursive(section, level, false));
             } else {
-                hull = CSG2D.union(hull, toCSGRecursive(section, level, false, flags));
+                hull = CSG2D.union(hull, toCSGRecursive(section, level, false));
             }
-            section = polSection(a, level, flags);
+            doneWith.addAll(section);
         }
 
         return hull;
@@ -121,22 +115,20 @@ class Polygon2Csg2dConverter {
 
     /**
      * Compute the convex hull of all the points in the list
-     * 
-     * @return list of point index pairs of the points on the hull
      */
-    List<Integer> convexHull(final List<Integer> subList) {
+    private static List<Point2D> convexHull(final List<Point2D> subList) {
         if (subList.size() < 3) {
             throw new RuntimeException("attempt to compute hull for " + subList.size() + " points!");
         }
 
-        final List<Integer> inConsideration = new ArrayList<Integer>(subList);
-        final List<Integer> result = createTriangle(inConsideration);
+        final List<Point2D> inConsideration = new ArrayList<Point2D>(subList);
+        final List<Point2D> result = createTriangle(inConsideration);
 
         while (inConsideration.size() > 0) {
             // for each side of the current result polygon, find the point that is farthest outside
-            Point2D p = listPoint(result.size() - 1, result);
+            Point2D p = result.get(result.size() - 1);
             for (int i = 0; i < result.size(); i++) {
-                final Point2D q = listPoint(i, result);
+                final Point2D q = result.get(i);
                 final int corner = findCorner(inConsideration, new HalfPlane(p, q));
                 if (corner != -1) {
                     result.add(i, inConsideration.remove(corner));
@@ -151,8 +143,8 @@ class Polygon2Csg2dConverter {
         return result;
     }
 
-    private List<Integer> createTriangle(final List<Integer> inConsideration) {
-        final List<Integer> result = new ArrayList<Integer>();
+    private static List<Point2D> createTriangle(final List<Point2D> inConsideration) {
+        final List<Point2D> result = new ArrayList<Point2D>();
         moveTopmostPoint(inConsideration, result);
         moveBottommostPoint(inConsideration, result);
         moveExtremalPoint(inConsideration, result);
@@ -161,11 +153,11 @@ class Polygon2Csg2dConverter {
         return result;
     }
 
-    private int findCorner(final List<Integer> inConsideration, final HalfPlane hp) {
+    private static int findCorner(final List<Point2D> inConsideration, final HalfPlane hp) {
         double vMax = 0;
         int corner = -1;
         for (int testPoint = inConsideration.size() - 1; testPoint >= 0; testPoint--) {
-            final double v = hp.value(listPoint(testPoint, inConsideration));
+            final double v = hp.value(inConsideration.get(testPoint));
             if (v >= vMax) {
                 vMax = v;
                 corner = testPoint;
@@ -174,12 +166,12 @@ class Polygon2Csg2dConverter {
         return corner;
     }
 
-    private void moveExtremalPoint(final List<Integer> inConsideration, final List<Integer> result) {
-        final HalfPlane hp = new HalfPlane(listPoint(0, result), listPoint(1, result));
+    private static void moveExtremalPoint(final List<Point2D> inConsideration, final List<Point2D> result) {
+        final HalfPlane hp = new HalfPlane(result.get(0), result.get(1));
         int extremal = 0;
         double extremum = 0;
         for (int i = 0; i < inConsideration.size(); i++) {
-            final double value = Math.abs(hp.value(listPoint(i, inConsideration)));
+            final double value = Math.abs(hp.value(inConsideration.get(i)));
             if (value > extremum) {
                 extremum = value;
                 extremal = i;
@@ -188,11 +180,11 @@ class Polygon2Csg2dConverter {
         movePoint(inConsideration, result, extremal);
     }
 
-    private void moveBottommostPoint(final List<Integer> inConsideration, final List<Integer> result) {
+    private static void moveBottommostPoint(final List<Point2D> inConsideration, final List<Point2D> result) {
         int bottom = 0;
         double yMin = Double.MAX_VALUE;
         for (int i = 0; i < inConsideration.size(); i++) {
-            final double y = listPoint(i, inConsideration).y();
+            final double y = inConsideration.get(i).y();
             if (y < yMin) {
                 yMin = y;
                 bottom = i;
@@ -201,11 +193,11 @@ class Polygon2Csg2dConverter {
         movePoint(inConsideration, result, bottom);
     }
 
-    private void moveTopmostPoint(final List<Integer> inConsideration, final List<Integer> result) {
+    private static void moveTopmostPoint(final List<Point2D> inConsideration, final List<Point2D> result) {
         int top = 0;
         double yMax = Double.MIN_VALUE;
         for (int i = 0; i < inConsideration.size(); i++) {
-            final double y = listPoint(i, inConsideration).y();
+            final double y = inConsideration.get(i).y();
             if (y > yMax) {
                 yMax = y;
                 top = i;
@@ -214,81 +206,76 @@ class Polygon2Csg2dConverter {
         movePoint(inConsideration, result, top);
     }
 
-    private static boolean movePoint(final List<Integer> inConsideration, final List<Integer> result, final int index) {
+    private static boolean movePoint(final List<Point2D> inConsideration, final List<Point2D> result, final int index) {
         return result.add(inConsideration.remove(index));
     }
 
-    private List<Integer> allPoints() {
-        final List<Integer> result = new ArrayList<Integer>();
+    private List<Point2D> allPoints() {
+        final List<Point2D> result = new ArrayList<Point2D>();
         for (int i = 0; i < polygon.size(); i++) {
-            result.add(new Integer(i));
+            result.add(polygon.point(i));
         }
         return result;
     }
 
     /**
-     * Get the next whole section to consider from list a
-     * 
-     * @return the section (null for none left)
+     * Find a indentation in the hull. That is a section of points that are not
+     * on the hull between two points on the hull. The result starts with a
+     * point on the hull, then follow one to many points not on the hull. At
+     * last comes a point on the hull again.
      */
-    static List<Integer> polSection(final List<Integer> a, final int level, final int[] flags) {
-        int flag, oldi;
-        oldi = a.size() - 1;
-        int oldFlag = flags[a.get(oldi).intValue()];
-
-        int ptr = -1;
-        for (int i = 0; i < a.size(); i++) {
-            flag = flags[a.get(i).intValue()];
-
-            if (flag < level && oldFlag >= level) {
-                ptr = oldi;
-                break;
-            }
-            oldi = i;
-            oldFlag = flag;
+    private static List<Point2D> findIndentation(final List<Point2D> points, final Set<Point2D> hullSet) {
+        if (points.size() <= hullSet.size()) {
+            throw new IllegalArgumentException("list of points to search must be bigger than the set of points on the hull");
         }
-
-        if (ptr < 0) {
-            return null;
+        if (hullSet.isEmpty()) {
+            throw new IllegalArgumentException("set of points on the hull cannot be empty");
         }
-
-        final List<Integer> result = new ArrayList<Integer>();
-        result.add(a.get(ptr));
-        ptr++;
-        if (ptr > a.size() - 1) {
-            ptr = 0;
-        }
-        while (flags[a.get(ptr).intValue()] < level) {
-            result.add(a.get(ptr));
+        final List<Point2D> result = new ArrayList<Point2D>();
+        int ptr = findFirstPointNotOnTheHull(points, hullSet);
+        result.add(wrapAroundGet(points, ptr - 1));
+        while (!hullSet.contains(wrapAroundGet(points, ptr))) {
+            result.add(wrapAroundGet(points, ptr));
             ptr++;
-            if (ptr > a.size() - 1) {
-                ptr = 0;
-            }
         }
-
-        result.add(a.get(ptr));
-
+        result.add(wrapAroundGet(points, ptr));
         return result;
     }
 
-    /**
-     * find a point from a list of polygon points
-     */
-    Point2D listPoint(final int i, final List<Integer> a) {
-        return polygon.point((a.get(i)).intValue());
+    private static int findFirstPointNotOnTheHull(final List<Point2D> points, final Set<Point2D> hullSet) {
+        int idx = findFirstPointOnTheHull(points, hullSet);
+        // no need to check for boundaries: points list is bigger than the hullSet
+        while (hullSet.contains(wrapAroundGet(points, idx))) {
+            idx++;
+        }
+        return idx;
+    }
+
+    private static int findFirstPointOnTheHull(final List<Point2D> points, final Set<Point2D> hullSet) {
+        int idx = 0;
+        // no need to check for boundaries: hullSet is included in the points list and is not empty
+        while (!hullSet.contains(wrapAroundGet(points, idx))) {
+            idx++;
+        }
+        return idx;
+    }
+
+    private static Point2D wrapAroundGet(final List<Point2D> a, final int ptr) {
+        final int size = a.size();
+        return a.get((ptr + size) % size);
     }
 
     /**
      * Put the points on a triangle (list a) in the right order
      */
-    private void clockWise(final List<Integer> a) {
+    private static void clockWise(final List<Point2D> a) {
         if (a.size() != 3) {
             throw new RuntimeException("cannot order clockWise a polygon that is not a triangle: " + a);
         }
-        final Point2D q = Point2D.sub(listPoint(1, a), listPoint(0, a));
-        final Point2D r = Point2D.sub(listPoint(2, a), listPoint(0, a));
+        final Point2D q = Point2D.sub(a.get(1), a.get(0));
+        final Point2D r = Point2D.sub(a.get(2), a.get(0));
         if (Point2D.op(q, r) > 0) {
-            final Integer k = a.get(0);
+            final Point2D k = a.get(0);
             a.set(0, a.get(1));
             a.set(1, k);
         }
@@ -297,12 +284,12 @@ class Polygon2Csg2dConverter {
     /**
      * Turn the list of hull points into a CSG convex polygon
      */
-    private CSG2D toCSGHull(final List<Integer> hullPoints) {
+    private static CSG2D toCSGHull(final List<Point2D> hullPoints) {
         Point2D p, q;
         CSG2D hull = CSG2D.universe();
-        p = listPoint(hullPoints.size() - 1, hullPoints);
+        p = hullPoints.get(hullPoints.size() - 1);
         for (int i = 0; i < hullPoints.size(); i++) {
-            q = listPoint(i, hullPoints);
+            q = hullPoints.get(i);
             hull = CSG2D.intersection(hull, new CSG2D(new HalfPlane(p, q)));
             p = q;
         }
@@ -313,22 +300,13 @@ class Polygon2Csg2dConverter {
     /**
      * Remove all the points in a list that are within or on the hull
      */
-    private void outsideHull(final List<Integer> inConsideration, final CSG2D hull) {
+    private static void outsideHull(final List<Point2D> inConsideration, final CSG2D hull) {
         final double small = Math.sqrt(MathRoutines.TINY_VALUE);
         for (int i = inConsideration.size() - 1; i >= 0; i--) {
-            final Point2D p = listPoint(i, inConsideration);
+            final Point2D p = inConsideration.get(i);
             if (hull.value(p) <= small) {
                 inConsideration.remove(i);
             }
-        }
-    }
-
-    /**
-     * Set all the flag values in a list the same
-     */
-    static void flagSet(final int f, final List<Integer> a, final int[] flags) {
-        for (int i = 0; i < a.size(); i++) {
-            flags[(a.get(i)).intValue()] = f;
         }
     }
 }
